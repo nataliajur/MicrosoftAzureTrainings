f4bb6c1ec6d287b31dc3003fa732aca4a3f8ef91
nataliajur <48248055+nataliajur@users.noreply.github.com>
2020-06-03 16:50:05 +0200

    Add files via upload

---
+++ ConnectingAzureSQLdbWithASP.NETWebApp_C#/CoursesAndModules.cs
@@ -0,0 +1,16 @@
+namespace CoursesWebApp.Models
+{
+    public class CoursesAndModules
+    {
+        public string CourseName { get; }
+        public string ModuleTitle { get; }
+        public int Sequence { get; }
+
+        public CoursesAndModules(string courseName, string moduleTitle, int sequence)
+        {
+            this.CourseName = courseName;
+            this.ModuleTitle = moduleTitle;
+            this.Sequence = sequence;
+        }
+    }
+}
